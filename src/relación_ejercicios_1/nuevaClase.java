4ef836cdfd9ff86dcfcab396bbb7ebc37766257c
Jother043 <91066713+Jother043@users.noreply.github.com>
2022-10-06 12:21:29 +0200

    Add files via upload

---
+++ src/relación_ejercicios_1/nuevaClase.java
@@ -0,0 +1,11 @@
+package relación_ejercicios_1;
+
+import java.util.Scanner;
+
+public class nuevaClase {
+
+    public static Scanner teclado = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.printf("Hola");
+    }
+}
